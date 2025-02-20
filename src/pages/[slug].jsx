f8b17ae162f5aa6d7d3156bc01d74c6d480c65d9
Leonardo Lüders <luderser@hotmail.com>
2025-02-20 17:34:49 -0300

    finished class 201 (getStaticPaths com 'fallback: true').

--- src/pages/[slug].jsx
+++ src/pages/[slug].jsx
@@ -1,8 +1,16 @@
 import P from 'prop-types';
+import { useRouter } from 'next/router';
 import { loadPages } from '@/api/load-pages';
 import { Home } from '@/templates/Home';
+import { Loading } from '@/templates/Loading';
 
 export default function Page({ data }) {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <Loading />;
+  }
+
   return <Home data={data} />;
 }
 
@@ -11,15 +19,15 @@ Page.propTypes = {
 };
 
 export const getStaticPaths = async () => {
-  const paths = (await loadPages()).map((page) => {
-    return {
-      params: { slug: page.slug },
-    };
-  });
+  // const paths = (await loadPages()).map((page) => {
+  //   return {
+  //     params: { slug: page.slug },
+  //   };
+  // });
 
   return {
-    paths,
-    fallback: false,
+    paths: [{ params: { slug: 'another-landing-page' } }],
+    fallback: true,
   };
 };
 
@@ -32,7 +40,7 @@ export const getStaticProps = async (ctx) => {
     console.log('getStaticProps from [slug].jsx error:', e);
   }
 
-  if (!data) {
+  if (!data || !data.length) {
     return {
       notFound: true,
     };
