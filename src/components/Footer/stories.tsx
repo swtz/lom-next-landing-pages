a4c433318796fd6282dc4d0fa79709c4aafb099d
Leonardo Lüders <luderser@hotmail.com>
2025-02-21 11:21:14 -0300

    types added to 'Footer' component.

--- src/components/Footer/stories.tsx
+++ src/components/Footer/stories.tsx
@@ -1,4 +1,5 @@
-import { Footer } from '.';
+import { Meta, StoryFn } from '@storybook/react/types-6-0';
+import { Footer, FooterProps } from '.';
 
 export default {
   title: 'Footer',
@@ -6,9 +7,9 @@ export default {
   args: {
     footerHtml: `<a rel="nofollow" target="_blank" href="https://github.com/swtz">Feito com ❤️ por L.Lüders</a>`,
   },
-};
+} as Meta;
 
-export const Template = (args) => {
+export const Template: StoryFn<FooterProps> = (args) => {
   return (
     <div>
       <Footer {...args} />
